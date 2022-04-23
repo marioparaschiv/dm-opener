de6c2d280c3341ec508c7c6db9847f643d892083
eternal <eternal.git@gmail.com>
2022-04-23 23:29:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ import config from './config.json' assert { type: 'json' };
 
 import { readdirSync, readFileSync } from 'fs';
 import fetch from 'node-fetch';
+import { join } from 'path';
 
 const files = readdirSync(process.argv[2]);
 for (const file of files) {
    if (file === 'index.json') continue;
 
    try {
-      const contents = readFileSync(`${process.argv[2]}\\${file}\\channel.json`);
+      const path = join(process.argv[2], file, 'channel.json');
+      const contents = readFileSync(path);
       const data = JSON.parse(contents);
 
       const recipients = [(data.recipients ?? []).filter(r => r !== config.userid)].filter(Boolean);
       if (recipients.length) {
-         const res = await fetch('https://canary.discordapp.com/api/v6/users/@me/channels', {
+         const res = await fetch('https://canary.discordapp.com/api/v9/users/@me/channels', {
             method: 'POST',
             body: JSON.stringify({ recipients }),
             headers: {
