120590aca86d68406859ac73a3b2f6aa42afadbf
eternal <eternal.git@gmail.com>
2022-04-23 23:14:15 +0100

    rewrite

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
+const { readDirSync, readFileSync } = require('fs');
 const config = require('./config.json');
 const fetch = require('node-fetch');
-const fs = require('fs');
 
-(async function () {
-   let files = await readDir(process.argv[2]);
-   for (const file of files) {
-      if (file === 'index.json') return;
-      let data = await readFile(`${process.argv[2]}\\${file}\\channel.json`);
-      data = JSON.parse(data);
-      let body = {
-         recipients: [data.recipients ? data.recipients.filter((r) => r !== config.userid)[0] : '']
-      };
-      if (body.recipients[0] !== '') {
-         let response = await fetch('https://canary.discordapp.com/api/v6/users/@me/channels', {
+const files = readDirSync(process.argv[2]);
+for (const file of files) {
+   if (file === 'index.json') continue;
+
+   try {
+      const contents = readFileSync(`${process.argv[2]}\\${file}\\channel.json`);
+      const data = JSON.parse(contents);
+
+      const recipients = [(data.recipients ?? []).filter(r => r !== config.userid)].filter(Boolean);
+      if (recipients.length) {
+         const res = await fetch('https://canary.discordapp.com/api/v6/users/@me/channels', {
             method: 'POST',
             body: JSON.stringify(body),
             headers: {
@@ -20,31 +20,11 @@ const fs = require('fs');
                'User-Agent': config.agent,
                'Content-Type': 'application/json'
             }
-         });
-         let res = await response.json();
+         }).then(r => r.json());
+
          console.log(`Opened DM with ${res.recipients[0].username}#${res.recipients[0].discriminator}`);
       }
+   } catch {
+      continue;
    }
-})();
-
-async function readFile(path) {
-   return new Promise((resolve, reject) => {
-      fs.readFile(path, function (err, data) {
-         if (err) {
-            reject(err);
-         }
-         resolve(data);
-      });
-   });
-}
-
-async function readDir(path) {
-   return new Promise((resolve, reject) => {
-      fs.readdir(path, function (err, data) {
-         if (err) {
-            reject(err);
-         }
-         resolve(data);
-      });
-   });
 }
