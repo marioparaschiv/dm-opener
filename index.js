58037c5c77fa287d7dd730d3bce3bbc41b7569b9
eternal <eternal.git@gmail.com>
2022-04-23 23:45:43 +0100

    more fixes

--- index.js
+++ index.js
@@ -1,33 +1,55 @@
-import config from './config.json' assert { type: 'json' };
-
-import { readdirSync, readFileSync } from 'fs';
+import { readdirSync, readFileSync, existsSync } from 'fs';
 import fetch from 'node-fetch';
 import { join } from 'path';
 
-const files = readdirSync(process.argv[2]);
-for (const file of files) {
-   if (file === 'index.json') continue;
+const config = JSON.parse(readFileSync(new URL('./config.json', import.meta.url)));
+const sleep = (time) => new Promise(r => setTimeout(r, time));
 
-   try {
-      const path = join(process.argv[2], file, 'channel.json');
-      const contents = readFileSync(path);
-      const data = JSON.parse(contents);
+if (!process.argv[2]) {
+   console.error('Please provide a file path pointing to the "messages" folder in your data package.');
+   process.exit(-1);
+}
 
-      const recipients = [(data.recipients ?? []).filter(r => r !== config.userid)].filter(Boolean);
-      if (recipients.length) {
-         const res = await fetch('https://canary.discordapp.com/api/v9/users/@me/channels', {
-            method: 'POST',
-            body: JSON.stringify({ recipients }),
-            headers: {
-               'authorization': config.token,
-               'User-Agent': config.agent,
-               'Content-Type': 'application/json'
-            }
-         }).then(r => r.json());
+if (!existsSync(process.argv[2])) {
+   console.error('The path you provided is not valid or doesn\'t exist.');
+   process.exit(-1);
+}
 
-         console.log(`Opened DM with ${res.recipients[0].username}#${res.recipients[0].discriminator}`);
+try {
+   run();
+} catch (e) {
+   console.log('Failed to open all your DMs: ', e.message);
+}
+
+async function run() {
+   const files = readdirSync(process.argv[2]);
+   for (const file of files) {
+      if (file === 'index.json') continue;
+
+      try {
+         const path = join(process.argv[2], file, 'channel.json');
+         const contents = readFileSync(path);
+         const data = JSON.parse(contents);
+
+         const recipients = (data.recipients ?? []).filter(r => r !== config.userid);
+         if (recipients.length) {
+            const res = await fetch('https://canary.discordapp.com/api/v6/users/@me/channels', {
+               method: 'POST',
+               body: JSON.stringify({ recipients }),
+               headers: {
+                  'authorization': config.token,
+                  'User-Agent': config.agent,
+                  'Content-Type': 'application/json'
+               }
+            }).then(r => r.json());
+
+            const { username, discriminator } = res.recipients[0];
+            console.log(`Opened DM with ${username}#${discriminator}`);
+            await sleep(250);
+         }
+      } catch (e) {
+         console.error('Failed to open DM', e.message);
+         continue;
       }
-   } catch {
-      continue;
    }
 }
